4f89b142f5d50aca86e8a6f4e6ccb84873ca7f50
nishant <momkingtai@gmail.com>
2026-07-16 11:26:51 +0530

    added monster file

--- monster.py
+++ monster.py
@@ -9,6 +9,7 @@ class Monster:
     name: str
     species: Species
     cpu: float
+    memory: int
 
     x: float
     y: float
@@ -16,10 +17,16 @@ class Monster:
     vx: float
     phase: float
 
-    def update(self, dt):
-        activity = min(self.cpu / 100.0, 1.0)
+    def update(self, dt, world_width):
+        activity = min(
+            self.cpu / 100.0,
+            1.0,
+        )
 
         self.x += self.vx * dt
+
+        if self.x >= world_width:
+            self.x = 0.0
 
         self.phase += dt * (
             2.0 + activity * 4.0
